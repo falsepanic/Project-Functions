edffd0f34adcd727e973499d12fd87bdd478dbb9
falsepanic <contact@falsepanic.com>
2017-03-02 15:49:01 -0600

    Revert "Room Modes"
    This reverts commit 41fb6b641737163dd19f657eae0a53ff013f7d94.

--- RT60.m
+++
@@ -1,6 +0,0 @@
-function [output] = RT60(alpha,volume,area)
-%RT60 Calculates the reverberation time (in seconds) of the room, based on 1125.33 ft/s for c.
-%   Format: RT60(alpha,volume,area) in FEET
-output = (0.049*volume)/(alpha*area);
-end
-
